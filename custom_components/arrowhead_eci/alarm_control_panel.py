82e5ebc20a727c0a8e534facfc577789796d1885
jon0riginal <g.antonym497@passmail.com>
2026-08-31 15:00:17 +1200

    Refactor: add utility function for device info

--- custom_components/arrowhead_eci/alarm_control_panel.py
+++ custom_components/arrowhead_eci/alarm_control_panel.py
@@ -10,7 +10,6 @@ from homeassistant.components.alarm_control_panel import (
 )
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
@@ -20,7 +19,7 @@ from custom_components.arrowhead_eci import (
     EciConfigModel,
 )
 
-from .const import DOMAIN
+from .util import get_device_info
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -57,7 +56,7 @@ class ArrowheadAlarmAreaControlPanel(CoordinatorEntity, AlarmControlPanelEntity)
         self.area_id = area_id
         self.coordinator: ArrowheadEciDataUpdateCoordinator = coordinator
 
-        self._attr_name = f"Eci Alarm Panel - {name}"
+        self._attr_name = f"Alarm Panel - {name}"
         self._attr_unique_id = f"area_{area_id}_alarm_panel"
 
         self._attr_supported_features = (
@@ -68,12 +67,7 @@ class ArrowheadAlarmAreaControlPanel(CoordinatorEntity, AlarmControlPanelEntity)
         self._attr_code_arm_required = False
         self._attr_code_disarm_required = True
         self._attr_available = is_enabled
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, f"eci_alarm_{config.serial_number}")},
-            manufacturer="Arrowhead Alarm Products",
-            name=f"Eci Alarm Panel - {config.serial_number}",
-            serial_number=config.serial_number,
-        )
+        self._attr_device_info = get_device_info(config)
 
     @property
     def alarm_state(self) -> AlarmControlPanelState:
---
+++ custom_components/arrowhead_eci/util.py
@@ -0,0 +1,13 @@
+from homeassistant.helpers.device_registry import DeviceInfo
+
+from .const import DOMAIN
+from .models import EciConfigModel
+
+
+def get_device_info(config: EciConfigModel):
+    return DeviceInfo(
+            identifiers={(DOMAIN, f"eci_alarm_{config.serial_number}")},
+            manufacturer="Arrowhead Alarm Products",
+            name=f"Eci Alarm Panel - {config.serial_number}",
+            serial_number=config.serial_number,
+        )
